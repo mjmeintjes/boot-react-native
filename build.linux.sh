9fbb2f37d1e507fd33bce09484a3eeee743fa28a
Matt Meintjes <matthys@mattsum.com>
2016-01-17 12:12:49 +1300

    Fix boot version

--- build.linux.sh
+++ build.linux.sh
@@ -339,20 +339,24 @@ travis_fold start before_script.8
 travis_fold end before_script.8
 
 travis_fold start before_script.9
-  travis_cmd \(mkdir\ \~/bin\ \&\&\ cd\ \~/bin\ \&\&\ curl\ -fsSLo\ boot\ https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh\ \&\&\ chmod\ 755\ boot\) --assert --echo --timing
+  travis_cmd export\ BOOT_VERSION\=2.5.5 --assert --echo --timing
 travis_fold end before_script.9
 
 travis_fold start before_script.10
-  travis_cmd ./gradlew\ assembleDebug\ -PdisablePreDex\ -Pjobs\=1 --assert --echo --timing
+  travis_cmd \(mkdir\ \~/bin\ \&\&\ cd\ \~/bin\ \&\&\ curl\ -fsSLo\ boot\ https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh\ \&\&\ chmod\ 755\ boot\) --assert --echo --timing
 travis_fold end before_script.10
 
 travis_fold start before_script.11
-  travis_cmd android-wait-for-emulator --assert --echo --timing
+  travis_cmd ./gradlew\ assembleDebug\ -PdisablePreDex\ -Pjobs\=1 --assert --echo --timing
 travis_fold end before_script.11
 
 travis_fold start before_script.12
-  travis_cmd adb\ shell\ input\ keyevent\ 82\ \& --assert --echo --timing
+  travis_cmd android-wait-for-emulator --assert --echo --timing
 travis_fold end before_script.12
+
+travis_fold start before_script.13
+  travis_cmd adb\ shell\ input\ keyevent\ 82\ \& --assert --echo --timing
+travis_fold end before_script.13
 
 travis_cmd cd\ ../..\ \&\&\ ls --echo --timing
 travis_result $?
