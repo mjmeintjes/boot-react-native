d6f1bc998f117c528f818b88b01c2d582380564e
Matt Meintjes <matthys@mattsum.com>
2016-01-17 00:15:36 +1300

    Add boot to path

--- build.linux.sh
+++ build.linux.sh
@@ -331,20 +331,24 @@ travis_fold start before_script.6
 travis_fold end before_script.6
 
 travis_fold start before_script.7
-  travis_cmd \(cd\ /usr/local/bin\ \&\&\ curl\ -fsSLo\ boot\ https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh\ \&\&\ chmod\ 755\ boot\) --assert --echo --timing
+  travis_cmd export\ PATH\=\~/bin:\$PATH --assert --echo --timing
 travis_fold end before_script.7
 
 travis_fold start before_script.8
-  travis_cmd ./gradlew\ :ReactAndroid:assembleDebug\ -PdisablePreDex\ -Pjobs\=1 --assert --echo --timing
+  travis_cmd \(mkdir\ \~/bin\ \&\&\ cd\ /\~/bin\ \&\&\ curl\ -fsSLo\ boot\ https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh\ \&\&\ chmod\ 755\ boot\) --assert --echo --timing
 travis_fold end before_script.8
 
 travis_fold start before_script.9
-  travis_cmd android-wait-for-emulator --assert --echo --timing
+  travis_cmd ./gradlew\ :ReactAndroid:assembleDebug\ -PdisablePreDex\ -Pjobs\=1 --assert --echo --timing
 travis_fold end before_script.9
 
 travis_fold start before_script.10
-  travis_cmd adb\ shell\ input\ keyevent\ 82\ \& --assert --echo --timing
+  travis_cmd android-wait-for-emulator --assert --echo --timing
 travis_fold end before_script.10
+
+travis_fold start before_script.11
+  travis_cmd adb\ shell\ input\ keyevent\ 82\ \& --assert --echo --timing
+travis_fold end before_script.11
 
 travis_cmd boot\ -v --echo --timing
 travis_result $?
