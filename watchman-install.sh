54252b5615ec08937e1fef601bf5b5e7b8f48141
Matt Meintjes <matthys@mattsum.com>
2016-01-17 21:46:01 +1300

    Fix? watchman install

--- watchman-install.sh
+++ watchman-install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/facebook/watchman.git
 cd watchman
 git checkout v4.1.0  # the latest stable release
 ./autogen.sh
-./configure
+mkdir -p ~/watchman
+./configure --enable-statedir=~/watchman
 make
 ln -s `pwd`/watchman ~/bin/watchman
