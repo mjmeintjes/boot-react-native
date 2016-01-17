a61f95e3226331029504d16af0ed8523988fa4ad
Matt Meintjes <matthys@mattsum.com>
2016-01-17 19:53:38 +1300

    Removed installDebug and use adb to install

--- build.linux.sh
+++ build.linux.sh
@@ -383,7 +383,7 @@ travis_fold start before_script.19
 travis_fold end before_script.19
 
 travis_fold start before_script.20
-  travis_cmd ./gradlew\ installDebug\ -PdisablePreDex\ -Pjobs\=1 --assert --echo --timing
+  travis_cmd adb\ install\ app/android/app/build/outputs/apk/app-debug.apk --assert --echo --timing
 travis_fold end before_script.20
 
 travis_fold start before_script.21
