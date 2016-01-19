6913d9acce95b06190b79868251dd73b160cc211
Matt Meintjes <matthys@mattsum.com>
2016-01-20 12:26:42 +1300

    Immediately mount root-view after start-up - Android

--- example/app/index.android.js
+++ example/app/index.android.js
@@ -4,48 +4,6 @@
  */
 'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+require('./build/main.js');
+mattsum.simple_example.core.main();
 
-var SimpleExampleApp = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-      </View>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('SimpleExampleApp', () => SimpleExampleApp);
-
-setTimeout(function(){
-    require('./build/main.js');
-    mattsum.simple_example.core.main();
-});
